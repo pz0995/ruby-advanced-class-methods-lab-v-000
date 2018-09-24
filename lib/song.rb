d1d0fb7f71a1bc8ce52e3e979bff2ac4aed231aa
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-24 03:03:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,25 +12,25 @@ class Song
     if !filename = end_with?(".mp3")
       puts "invalid format - song must end with '.mp3'"
     end
-# 
+#
 # Build a class constructor Song.new_by_name that takes in the string name of a song and returns a song instance with that name set as its name
 # property. Song.new_by_name should return an instance of Song and not a simple string or anything else. Implement the following functionality:
   end
 
   def self.create_by_name
-# instantiates and saves a song with a name property
-Song.create_by_name that takes in the string name of a song and returns a song instance with that name set as its name property and the song being saved into the @@all class variable
+# # instantiates and saves a song with a name property
+# Song.create_by_name that takes in the string name of a song and returns a song instance with that name set as its name property and the song being saved into the @@all class variable
   end
 
   def self.find_by_name
     find a song
-    ng present in @@all by name
-    Build a class finder Song.find_by_name that accepts the string name of a song and returns the matching instance of the song with that name. Consider:
+    # ng present in @@all by name
+    # Build a class finder Song.find_by_name that accepts the string name of a song and returns the matching instance of the song with that name. Consider:
   end
 
   def self.find_or_create_by_name
     .find_by_name && .create_by_name instead of repeating code
-# 
+#
 # In order to prevent duplicate songs being created that actually represent the same song (based on the song name), we're
 #  going to build a Song.find_or_create_by_name class method. This method will accept a string name for a song and either return a matching song instance with that name or create a new song with the name and return the song instance.end'
 
